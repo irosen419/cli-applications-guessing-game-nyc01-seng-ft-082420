d1f37b9c30e3217261f813e0ee27bf0b43e61715
Ian Rosen <irosen419@gmail.com>
2020-05-02 16:05:20 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -17,19 +17,24 @@ def user_input
     return input.to_i
   end
 end
-
-# Runs the full guessing game, with targeted outputs based on results
-def run_guessing_game
-  comp_num = computer_num
-  prompt_user
-  user_num = user_input
-  
+# Copmares the inputs from user and copmuter
+# If user input is incorrect, allows user to choose until correct
+def compare_inputs (user_num, comp_num)
   if user_num == comp_num
     puts "You guessed the correct number!"
   elsif user_num == "exit"
     puts "Goodbye!"
   else
-    puts "Sorry! The computer guessed #{comp_num}."
+    puts "Sorry! The computer guessed another number."
+    prompt_user
+    user_num = user_input
+    compare_inputs(user_num, comp_num)
   end
-  
+end
+# Runs the full guessing game, with targeted outputs based on results
+def run_guessing_game
+  comp_num = computer_num
+  prompt_user
+  user_num = user_input
+  compare_inputs(user_num, comp_num)
 end
